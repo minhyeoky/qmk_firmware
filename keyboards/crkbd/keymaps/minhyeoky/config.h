fea1ae93be11a1efa2fba6083116c15a92767ec9
Minhyeok Lee <minhyeok.lee95@gmail.com>
2023-08-31 11:04:01 +0900

    Revert "Redesign symbol layer"
    This reverts commit ca79e9f4948e90e14b555372fc83c1e8d7ab8785.

--- keyboards/crkbd/keymaps/minhyeoky/config.h
+++ keyboards/crkbd/keymaps/minhyeoky/config.h
@@ -20,42 +20,48 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+//#define USE_MATRIX_I2C
+
 /* Select hand configuration */
 
 #define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+//#define QUICK_TAP_TERM 0
+//#define TAPPING_TERM 100
 
 #ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
-#    define RGBLIGHT_LIMIT_VAL 120
-#    define RGBLIGHT_HUE_STEP 10
-#    define RGBLIGHT_SAT_STEP 17
-#    define RGBLIGHT_VAL_STEP 17
+    #define RGBLIGHT_EFFECT_BREATHING
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    #define RGBLIGHT_EFFECT_SNAKE
+    #define RGBLIGHT_EFFECT_KNIGHT
+    #define RGBLIGHT_EFFECT_CHRISTMAS
+    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #define RGBLIGHT_EFFECT_RGB_TEST
+    #define RGBLIGHT_EFFECT_ALTERNATING
+    #define RGBLIGHT_EFFECT_TWINKLE
+    #define RGBLIGHT_LIMIT_VAL 120
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
 #endif
+//
+#        define PERMISSIVE_HOLD
+#        define TAPPING_TERM 150
 
-/* Tap Hold */
-#define PERMISSIVE_HOLD
-#define TAPPING_TERM 100
-
-#define ONESHOT_TAP_TOGGLE 3 // Use 0 to disable
-#define ONESHOT_TIMEOUT 5000 // ms
-#define COMBO_TERM 20        // ms
+#        define ONESHOT_TAP_TOGGLE 3  // Use 0 to disable
+#        define ONESHOT_TIMEOUT 5000  // ms
+#        define COMBO_TERM 20         // ms
 
 // Mouse key speed and acceleration.
-#undef MOUSEKEY_DELAY
-#define MOUSEKEY_DELAY 0
-#undef MOUSEKEY_INTERVAL
-#define MOUSEKEY_INTERVAL 8
-#undef MOUSEKEY_WHEEL_DELAY
-#define MOUSEKEY_WHEEL_DELAY 0
-#undef MOUSEKEY_MAX_SPEED
-#define MOUSEKEY_MAX_SPEED 4
-// #    endif
+#        undef MOUSEKEY_DELAY
+#        define MOUSEKEY_DELAY          0
+#        undef MOUSEKEY_INTERVAL
+#        define MOUSEKEY_INTERVAL       8
+#        undef MOUSEKEY_WHEEL_DELAY
+#        define MOUSEKEY_WHEEL_DELAY    0
+#        undef MOUSEKEY_MAX_SPEED
+#        define MOUSEKEY_MAX_SPEED      4
+//#    endif
